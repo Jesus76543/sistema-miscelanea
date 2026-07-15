ec981a06327a7d2e3d507f9736759d2f02d0654c
Jesus76543 <jesus_3041230346@utd.edu.mx>
2026-07-15 00:09:37 -0600

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/rivera_bracho_jesusramiro_proyecto_final/sistema-miscelanea/dist/',
   plugins: [react()],
 })
